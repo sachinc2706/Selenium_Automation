3beda73171ca70f5464174b79167e7b54a7e0397
arnk086 <arnk086@gmail.com>
2022-08-27 19:59:25 +0530

    initial commit

---
+++ Selenium_Automation/src/test/java/Test1.java
@@ -0,0 +1,8 @@
+
+public class Test1 {
+
+	private void psvm() {
+		// TODO Auto-generated method stub
+		System.out.println("hi");
+	}
+}
---
+++ Selenium_Automation/src/test/java/com/FirstScript.java
@@ -0,0 +1,45 @@
+package com;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeGroups;
+import org.testng.annotations.BeforeTest;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class FirstScript {
+
+	public static WebDriver driver;
+
+	public static void main(String[] args) {
+
+		WebDriverManager.chromedriver().setup();
+
+		driver = new ChromeDriver();
+
+		driver.get("https://www.selenium.dev/selenium/web/web-form.html");
+
+		String title = driver.getTitle();
+
+		driver.manage().timeouts().implicitlyWait(Duration.ofMillis(500));
+
+		WebElement textBox = driver.findElement(By.name("my-text"));
+		WebElement submitButton = driver.findElement(By.cssSelector("button"));
+
+		textBox.sendKeys("Selenium");
+		submitButton.click();
+
+		WebElement message = driver.findElement(By.id("message"));
+		String value = message.getText();
+
+		System.out.println("Value->  " + value);
+		
+		driver.quit();
+
+	}
+
+}
