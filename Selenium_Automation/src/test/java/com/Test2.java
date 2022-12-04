6e65bf6aa0f8db649d9767c702e66b4eedacdaba
arnk086 <arnk086@gmail.com>
2022-12-04 18:54:31 +0530

    added pom.xml testng.xml test2.java

--- Selenium_Automation/src/test/java/com/Test2.java
+++ Selenium_Automation/src/test/java/com/Test2.java
@@ -1,9 +1,53 @@
+
 package com;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeGroups;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class Test2 {
 
-	private void psvm() {
-		// TODO Auto-generated method stub
-		System.out.println("hi");
+	public static  WebDriver driver;
+
+	@Test
+	public void fun() {
+
+		WebDriverManager.chromedriver().setup();
+
+		driver = new ChromeDriver();
+
+		driver.get("https://www.selenium.dev/selenium/web/web-form.html");
+
+		String title = driver.getTitle();
+
+		driver.manage().timeouts().implicitlyWait(Duration.ofMillis(500));
+
+		WebElement textBox = driver.findElement(By.name("my-text"));
+		WebElement submitButton = driver.findElement(By.cssSelector("button"));
+
+		textBox.sendKeys("Selenium");
+		submitButton.click();
+
+		WebElement message = driver.findElement(By.id("message"));
+		String value = message.getText();
+
+		System.out.println("Value->  " + value);
+
+		driver.quit();
+
 	}
+	
+	@Test
+	public void printText() {
+		System.out.println("*********************** Hi printText method executed *************************");
+	}
+
 }
